d19c0296df90e7dc77c45811d5af38f1359f341f
ywind <guoshukang@vip.qq.com>
2015-07-13 22:23:05 +0800

    update TextView

--- src/com/github/ywind/view/TextView.java
+++ src/com/github/ywind/view/TextView.java
@@ -1,5 +1,7 @@
 package com.github.ywind.view;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,12 +14,42 @@ import javax.servlet.http.HttpServletResponse;
  * 
  */
 public class TextView implements View {
-
+	private String string;
+	private String contentType;
+	private String characterEncoding;
+	
+	public TextView(String string) {
+		this.string=string;
+	}
+	
+	public TextView(String string,String contentType) {
+		this.string=string;
+		this.contentType=contentType;
+	}
+	
+	public TextView(String string,String contentType,String characterEncoding) {
+		this.string=string;
+		this.contentType=contentType;
+		this.characterEncoding=characterEncoding;
+	}
+	
 	@Override
 	public void render(Map<String, ?> models, HttpServletRequest request,
 			HttpServletResponse response) {
-		// TODO Auto-generated method stub
-
+        StringBuilder sb = new StringBuilder(64);
+        sb.append(contentType==null ? "text/html" : contentType)
+          .append(";charset=").append(characterEncoding==null ? "UTF-8" : characterEncoding);
+        response.setContentType(sb.toString());
+        PrintWriter pw;
+		try {
+			pw = response.getWriter();
+			pw.write(string);
+	        pw.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
 	}
 
 }
